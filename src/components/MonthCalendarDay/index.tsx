936fef0800e616303e548ff7b843d0a34eeff816
daria_borodina <dasha.borodina2003@mail.ru>
2023-05-10 23:29:15 +0300

    fix: add test folder to tsconfig

--- src/components/MonthCalendarDay/index.tsx
+++ src/components/MonthCalendarDay/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
 import { usePopup } from '@/hooks';
-import { CalendarHelper } from '@/utils/CalendarHelper';
-import { DateValidator } from '@/utils/DateValidator';
-import { getFromLocalStorage } from '@/utils/localStorage';
+import { DateCellService } from '@/utils/DayCellService';
 
 import { ToDoList } from '../ToDoList';
-import { ToDo } from '../ToDoList/types';
 import { Cell, Dot, Wrapper } from './styles';
 import { MonthCalendarDayProps } from './types';
 
@@ -26,64 +23,21 @@ export const MonthCalendarDay: React.FC<MonthCalendarDayProps> = ({
 }) => {
   const [popUp, showPopup, setShowPopup] = usePopup();
 
-  const currentDate = new Date(Date.now());
-
-  const isCurrent = CalendarHelper.isDatesEqual(date, currentDate);
-  const isSelected = CalendarHelper.isDatesEqual(date, selectedDate);
-  const isSelectedMonth = date.getMonth() === panelMonth;
-  const isInValidRange = DateValidator.isInValidRange(date, minDate, maxDate);
-
-  let isWeekend = false;
-  if (showWeekends) {
-    isWeekend = date.getDay() === 0 || date.getDay() === 6;
-  }
-
-  let isHoliday = false;
-  if (holidays) {
-    const holiday = holidays.find((holiday) =>
-      CalendarHelper.isDatesEqual(holiday, date)
-    );
-    isHoliday = holiday ? true : false;
-  }
-
-  let isEndValue = false;
-  let isStartValue = false;
-  let isInRange = false;
-
-  if (endDate && CalendarHelper.isDatesEqual(endDate, date)) {
-    isEndValue = true;
-  }
-  if (isSelected && startDate) {
-    isEndValue = true;
-  }
-
-  if (startDate && CalendarHelper.isDatesEqual(startDate, date)) {
-    isStartValue = true;
-  }
-  if (isSelected && endDate) {
-    isStartValue = true;
-  }
-  if (!isSelected && !isStartValue && !isEndValue && selectedDate) {
-    if (
-      startDate &&
-      DateValidator.isInValidRange(date, startDate, selectedDate)
-    ) {
-      isInRange = true;
-    }
-
-    if (endDate && DateValidator.isInValidRange(date, selectedDate, endDate)) {
-      isInRange = true;
-    }
-  }
-
-  let hasTodos = false;
-  const todos = getFromLocalStorage(`todos${date.getTime()}`) as ToDo[];
-  if (todos && todos.length > 0) {
-    hasTodos = true;
-  }
+  const dateService = new DateCellService(
+    date,
+    selectedDate,
+    panelMonth,
+    showWeekends,
+    holidays,
+    minDate,
+    maxDate,
+    endDate,
+    startDate
+  );
+  const settings = dateService.getSettings();
 
   const handleClick = () => {
-    if (isInValidRange) {
+    if (settings.isInValidRange) {
       onClick(date);
     }
   };
@@ -102,20 +56,8 @@ export const MonthCalendarDay: React.FC<MonthCalendarDayProps> = ({
 
   return (
     <Wrapper ref={popUp} onMouseEnter={onMouseEnter} onMouseLeave={onMouseOver}>
-      <Cell
-        aria-label="cell"
-        onClick={handleClick}
-        isSelected={isSelected}
-        isCurrent={isCurrent}
-        isSelectedMonth={isSelectedMonth}
-        isWeekend={isWeekend}
-        isHoliday={isHoliday}
-        isInValidRange={isInValidRange}
-        isInRange={isInRange}
-        isEndValue={isEndValue}
-        isStartValue={isStartValue}
-      >
-        {hasTodos && <Dot>.</Dot>}
+      <Cell aria-label="cell" onClick={handleClick} {...settings}>
+        {settings.hasTodos && <Dot>.</Dot>}
         {date.getDate()}
       </Cell>
       {showPopup && <ToDoList date={date} index={index} />}
